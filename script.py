ed70708ad6a9a7502094caebd6eb361978acd926
AmAdwaith2006 <95268686+AmAdwaith2006@users.noreply.github.com>
2022-07-03 21:53:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,14 +4,14 @@ class script(object):
 <b>Send me any Telegram file and choose appropriate option! </b>"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
-    DOWNLOAD_START = "Give Me Some Time..."
+    UPGRADE_TEXT = "CONTACT @Varma2006"
+    DOWNLOAD_START = "Download Started..."
     UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
-    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > @ThoughtsByVarma**"
+    SAVED_THUMB = "Thumbnail Saved Permanently"
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!"
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully , Time To Upload⚡"
     CUSTOM_CAPTION_UL_FILE = " "
     HELP_USER = """It's not that complicated😅
     
